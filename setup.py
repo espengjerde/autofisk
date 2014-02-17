a623fb260e8f6420d29426bf95c329fadc2fde47
= <=>
2014-02-17 23:39:16 +0100

    struktur

---
+++ setup.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='AutoFisk',
+    version='0.0.1',
+    author='espengjerde',
+    author_email='espen.gjerde@gmail.com',
+    packages='AutoFisk',
+    url='http://github.com/espengjerde/autofisk',
+    license='LICENSE',
+    description='Automatic Fishing Vessel - Remote Controlled'
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
